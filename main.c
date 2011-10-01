f950558100902db9882d0ef9a686c21a14736be3
Karl Backström <backstrom@akafugu.jp>
2011-10-01 16:42:40 +0900

    Update company name

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
  * uA-thermometer for ATTiny13
- * (C) 2011 Akafugu
+ * (C) 2011 Akafugu Corporation
  *
  * This program is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
